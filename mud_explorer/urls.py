1b9c7dd68eebd55854466b368535b324cb05b7cd
mikaelacurrier <mikaela.currier@gmail.com>
2019-08-01 11:34:04 -0400

    :bug: Added trailing slashes to endpoints

--- mud_explorer/urls.py
+++ mud_explorer/urls.py
@@ -3,18 +3,18 @@ from django.urls import path
 from .views import init_pass, init_move, init_take, init_drop, init_sell, init_confirm_sell, init_status, init_examine, init_change_name, init_pray, init_flight, init_dash
 
 urlpatterns = [
-    path('init', init_pass, name="game-init"),
-    path('move', init_move, name="game-move"),
-    path('take', init_take, name="game-take"),
-    path('drop', init_drop, name="game-drop"),
-    path('sell', init_sell, name="game-sell"),
-    path('sell/confirm', init_confirm_sell, name="game-sell-confirm"),
-    path('status', init_status, name="game-status"),
-    path('examine', init_examine, name="game-examine"),
-    path('change_name', init_change_name, name="game-change_name"),
-    path('pray', init_pray, name="game-pray"),
-    path('fly', init_flight, name="game-fly"),
-    path('dash', init_dash, name="game-dash"),
+    path('init/', init_pass, name="game-init"),
+    path('move/', init_move, name="game-move"),
+    path('take/', init_take, name="game-take"),
+    path('drop/', init_drop, name="game-drop"),
+    path('sell/', init_sell, name="game-sell"),
+    path('sell/confirm/', init_confirm_sell, name="game-sell-confirm"),
+    path('status/', init_status, name="game-status"),
+    path('examine/', init_examine, name="game-examine"),
+    path('change_name/', init_change_name, name="game-change_name"),
+    path('pray/', init_pray, name="game-pray"),
+    path('fly/', init_flight, name="game-fly"),
+    path('dash/', init_dash, name="game-dash"),
 
 
 
